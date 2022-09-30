d88fbc8d776f2740ce445d505a1fb1824b32f344
Heliardo Sousa <heliardosousa159@outlook.com>
2022-09-30 17:13:53 -0300

    finalizado a lista de exercicio 04

--- ListaExercicio04/ListaExercicio04/Program.cs
+++ ListaExercicio04/ListaExercicio04/Program.cs
@@ -48,18 +48,18 @@
             {
                 Console.WriteLine("Digite o valor da posicao "+i+": ");
                 vetor[i] = int.Parse(Console.ReadLine());
-                for(int j = 1; j <= vetor[i]; j++)
+                for(int j = 2; j <= vetor[i]; j++)
                 {
                     if(vetor[i] % j == 0)
                     {
                         qtdDiv++;
                     }
                 }
-                if(qtdDiv == 2)
+                if(qtdDiv == 1)
                 {
-                    Console.WriteLine("Indice " + i);
-                    qtdDiv = 0;
+                    Console.WriteLine("Primo, Indice " + i);
                 }
+                qtdDiv = 0;
             }
             */
 
@@ -197,9 +197,37 @@
             }
             */
 
+            /*
             //QUESTAO 10
-            //int[] vetor = new vetor[20];
+            int[] vetor = new int[5];
+            int[] vetorResultado = new int[5];
 
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("Digite o valor da posicao "+(i+1)+": ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            //MOSTRANDO VETOR
+            Console.WriteLine("Vetor:");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(" " + vetor[i]);
+                if(vetor[i] == 0)
+                {
+                    vetorResultado[i] = 2 ;
+                }
+                else
+                {
+                    vetorResultado[i] = vetor[i];
+                }
+            }
+            //MOSTRANDO VETOR RESULTADO
+            Console.WriteLine("\nVetor Resultado:");
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(" " + vetorResultado[i]);
+            }
+            */
 
             /*
             //QUESTAO 11
@@ -243,18 +271,192 @@
             if (ocorre == 0) Console.WriteLine("Numero nao encontrado");
             */
 
+            /*
             //QUESTAO 13
+            int[] vetor = new int[100];
+            int cont = 0;
 
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("Digite: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if(vetor[i] == 2 || vetor[i] == 4 || vetor[i] == 8)
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine($"Os numero 2, 4 e 8 aparecem {cont} vezes");
+            */
 
+            /*
             //QUESTAO 14
+            int codigo = 0;
+            int[] vetor = new int[5];
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("Digite: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
 
+            do
+            {
+                Console.WriteLine("Digite um codigo: ");
+                codigo = int.Parse(Console.ReadLine());
+                switch (codigo)
+                {
+                    case 0:
+                        Console.WriteLine("Encerrando o programa!");
+                        break;
+                    case 1:
+                        Console.WriteLine("Vetor em ordem normal: ");
+                        for (int i = 0; i < vetor.Length; i++)
+                        {
+                            Console.Write(" " + vetor[i]);
+                        }
+                        Console.WriteLine("\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("vetor em ordem reversa: ");
+                        for (int i = 0; i < vetor.Length; i++)
+                        {
+                            Console.Write(" " + vetor[vetor.Length - (i + 1)]);
+                        }
+                        Console.WriteLine("\n");
+                        break;
+                    default:
+                        Console.WriteLine("Opcao invalida");
+                        break;
+                }  
+            } while (codigo != 0);
+            */
 
+            /*
             //QUESTAO 15
+            int[] vetor = new int[5];
+            int[] vetorInvertido = new int[5];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("Digite: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetorInvertido[i] = vetor[vetor.Length - (i+1)];
+            }
+            Console.WriteLine("Vetor normal:");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write($" {vetor[i]}");
+            }
+            Console.WriteLine("\nVetor invertido:");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write($" {vetorInvertido[i]}");
+            }
+            */
 
 
             //QUESTAO 16
-            //int[] x = new int[10], y = new int[10];
+            int[] x = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] y = new int[10] { 1, 6, 30, 4, 5, 60, 7, 9, 10, 11 };
+            int[] uniao = new int[20];
+            int[] diferenca = new int[10];
+            int[] interseccao = new int[10];
+            Console.WriteLine("UNIÃO");
+            x.CopyTo(uniao, 0);
+            int auxU = 10;
+            //                        0  1  2  3  4  5  6  7  8   9            
+            //int[] x = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };            
+            //int[] y = new int[10] { 1, 6,30, 4, 5,60, 7, 9,10, 11 };
+            for (int i = 0; i < y.Length; i++)
+            {
+                //elementos de x e elementos y que não estão em X            
+                for (int j = 0; j < x.Length; j++)
+                {
+                    if (y[i] == x[j])
+                    {
+                        break;
+                    }
+                    if (j == (x.Length - 1))
+                    {
+                        uniao[auxU] = y[i];
+                        auxU++;
+                    }
+                }
+            }
+            for (int i = 0; i<auxU; i++)
+            {
+                Console.WriteLine(uniao[i]);
+            }
+            Console.WriteLine("");
 
+
+            Console.WriteLine("DIFERENÇA");
+            //diferença - elementos de X que não estão no Y            
+            //                        0  1  2  3  4  5  6  7  8   9            
+            //int[] x = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };            
+            //int[] y = new int[10] { 1, 6,30, 4, 5,60, 7, 9,10, 11 };
+            int auxD = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = 0; j < y.Length; j++)
+                {
+                    if (x[i] == y[j])
+                    {
+                        break;
+                    }
+                    if (j == (y.Length - 1))
+                    {
+                        diferenca[auxD] = x[i];
+                        auxD++;
+                    }
+                }
+            }
+            for (int i = 0; i < auxD; i++)
+            {
+                Console.WriteLine(diferenca[i]);
+            }
+            Console.WriteLine("");
+
+
+            Console.WriteLine("INTERSECCAO");
+            int auxI = 0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                for(int j = 0; j < y.Length; j++)
+                {
+                    if(x[i] == y[j])
+                    {
+
+                    }
+                }
+            }
+
+            Console.WriteLine("INTERSECÇÃO");
+            //os elementos que aparecem em X e Y
+            auxI = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = 0; j < y.Length; j++)
+                {
+                    if (x[i] == y[i])
+                    {
+                        interseccao[auxI] = x[i];
+                        auxI++;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < auxI; i++)
+            {
+                Console.WriteLine(interseccao[i]);
+            }
+            Console.WriteLine("");
 
         }
     }
